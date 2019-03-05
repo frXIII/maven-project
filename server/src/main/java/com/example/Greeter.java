327904b631a318d60cef079e195b7211347acce4
frXIII <48213842+frXIII@users.noreply.github.com>
2019-03-05 14:50:19 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,7 +8,7 @@ public class Greeter {
 
   }
 
-  public final String greet(final String someone) {
+  public final String greet(String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
 }
